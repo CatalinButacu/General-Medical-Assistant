eab4d3654b31613cd0be0d2254351e57460f6721
Catalin.BUTACU <ic.butacu@lectra.com>
2025-10-10 14:14:25 +0300

    Restore routes/__init__.py: Add blueprint imports for all route modules

--- ml_backend/routes/__init__.py
+++ ml_backend/routes/__init__.py
@@ -1,1 +1,11 @@
-# Routes package
+"""
+Routes package initialization
+Imports all blueprint modules for the Flask application
+"""
+
+from .auth import auth_bp
+from .chat import chat_bp
+from .health_profile import health_profile_bp
+from .medicine import medicine_bp
+
+__all__ = ['auth_bp', 'chat_bp', 'health_profile_bp', 'medicine_bp']
